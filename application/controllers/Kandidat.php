db0c27cf6c5a9d2c8824dfc75063fcd29b6086a3
tsabit12 <tsabit830@gmail.com>
2022-05-29 22:01:16 +0700

    closes #3 completed all service for interview

--- application/controllers/Kandidat.php
+++ application/controllers/Kandidat.php
@@ -38,6 +38,128 @@ class Kandidat extends  REST_Controller{
 
           $this->response($response, 200);
      }
+
+     public function berkas_get(){
+          $response['status']      = false;
+          $response['message']     = 'Internal server error';
+
+          $params = $this->get();
+
+          if(isset($params['username'])){
+               $data = $this->model_kandidat->berkas($params['username']);
+               if($data['success']){
+                    $response['status'] = true;
+                    $response['message'] = "Ok";
+                    $response['result'] = $data['data'];
+               }
+          }else{
+               $response['message'] = 'Username is required';
+          }
+
+          $this->response($response, 200);
+     }
+
+     public function berkas_post(){
+          $response['status']      = false;
+          $response['message']     = 'Internal server error';
+
+          $payload = $this->post();
+          if(isset($payload['berkasid'])){
+               $config = array(
+                    array('field' => 'berkasid', 'label' => 'Berkas ID', 'rules' => 'required|max_length[3]'),
+                    array('field' => 'nilai', 'label' => 'Nilai', 'rules' => 'required|integer'),
+                    array('field' => 'username', 'label' => 'Username', 'rules' => 'required|max_length[30]'),
+               );
+
+               $this->form_validation->set_data($payload);
+               $this->form_validation->set_rules($config);
+
+               if($this->form_validation->run() === FALSE){
+                    $msg_arr            = $this->form_validation->error_array();
+                    $keys                = array_keys($msg_arr);
+                    $response['message'] = $msg_arr[$keys[0]];
+               }else{
+                    $update = $this->model_kandidat->updateberkas($payload);
+                    if($update['success']){
+                         $response['status'] = true;
+                         $response['message'] = "ok";
+                    }else{
+                         $response['message']     = 'Update failed';    
+                    }
+               }
+          }else{
+               $response['message']     = 'Berkas id is required';
+          }
+
+          $this->response($response, 200);
+     }
+
+     public function pks_post(){
+          $response['status']      = false;
+          $response['message']     = 'Internal server error';
+
+          $payload = $this->post();
+          if(isset($payload['username'])){
+               $config = array(
+                    array('field' => 'username', 'label' => 'Username', 'rules' => 'required|max_length[30]'),
+                    array('field' => 'judul', 'label' => 'Judul', 'rules' => 'required|max_length[100]'),
+                    array('field' => 'nopks', 'label' => 'Nomor PKS', 'rules' => 'required|max_length[70]'),
+                    array('field' => 'mulai', 'label' => 'Tanggal Mulai', 'rules' => 'required|max_length[10]'),
+                    array('field' => 'selesai', 'label' => 'Tanggal Selesai', 'rules' => 'required|max_length[10]'),
+               );
+
+               $this->form_validation->set_data($payload);
+               $this->form_validation->set_rules($config);
+               if($this->form_validation->run() === FALSE){
+                    $msg_arr            = $this->form_validation->error_array();
+                    $keys                = array_keys($msg_arr);
+                    $response['message'] = $msg_arr[$keys[0]];
+               }else{
+                    $add = $this->model_kandidat->addpks($payload);
+                    if($add['success']){
+                         $response['status']      = true;
+                         $response['message']     = 'Ok';    
+                    }else{
+                         $response['message']     = "INSERT FAILED";
+                    }
+               }
+          }else{
+               $response['message']     = 'Username id is required';
+          }
+
+          $this->response($response, 200);
+     }
+
+     public function tidaklulus_post(){
+          $response['status']      = false;
+          $response['message']     = 'Internal server error';
+
+          $payload = $this->post();
+          if(isset($payload['username'])){
+               $config = array(
+                    array('field' => 'username', 'label' => 'Username', 'rules' => 'required|max_length[30]')
+               );
+               $this->form_validation->set_data($payload);
+               $this->form_validation->set_rules($config);
+               if($this->form_validation->run() === FALSE){
+                    $msg_arr            = $this->form_validation->error_array();
+                    $keys                = array_keys($msg_arr);
+                    $response['message'] = $msg_arr[$keys[0]];
+               }else{
+                    $post = $this->model_kandidat->tidaklulus($payload);
+                    if($post['success']){
+                         $response['status']      = true;
+                         $response['message']     = 'Ok';    
+                    }else{
+                         $response['message']     = "UPDATE FAILED";
+                    }
+               }
+          }else{
+               $response['message']     = 'Username id is required';    
+          }
+
+          $this->response($response, 200);
+     }
 }
 
 ?>
--- application/models/Model_kandidat.php
+++ application/models/Model_kandidat.php
@@ -23,5 +23,85 @@ class Model_kandidat extends CI_Model {
 
           return $this->db->get()->result_array();
      }
+
+     public function berkas($username)
+     {
+          $result['success'] = false;
+
+          $this->db->select('a.berkasid, a.file_name, b.keterangan, a.nilai');
+          $this->db->from('t_berkas_mitra a');
+          $this->db->join('r_berkas b', 'a.berkasid = b.berkasid');
+          $this->db->where('a.username', $username);
+
+          $q = $this->db->get();
+          if($q->num_rows() > 0){
+               $result['success'] = true;
+               $result['data'] = $q->result_array();
+          }
+
+          return $result;
+     }
+
+     public function updateberkas($payload){
+          $result['success'] = false;
+
+          $this->db->where(array(
+               'berkasid' => $payload['berkasid'],
+               'username' => $payload['username']
+          ));
+
+          $this->db->update('t_berkas_mitra', array(
+               'nilai' => $payload['nilai']
+          ));
+
+          if($this->db->affected_rows() > 0){
+               $result['success'] = true;
+          }
+
+          return $result;
+     }
+
+     public function addpks($body){
+          $result['success'] = false;
+
+          $data = array(
+               'id_mitra' => $body['username'],
+               'no_pks' => $body['nopks'],
+               'judul_pks' => $body['judul'],
+               'tgl_mulai' => $body['mulai'],
+               'tgl_selesai' => $body['selesai']
+          );
+
+          $this->db->insert('t_pks', $data);
+          if($this->db->affected_rows() > 0){
+               $this->db->where('username', $body['username']);
+               $this->db->update('t_mitra', array(
+                    'status' => 'S4',
+                    'password' => md5($body['username'].'AAaa123$'),
+               ));
+               if($this->db->affected_rows() > 0){
+                    $result['success'] = true;
+               }else{
+                    //rolback
+                    $this->db->where('id_mitra', $body['username']);
+                    $this->db->delete('t_pks');
+               }
+          }
+
+          return $result;
+     }
+
+     public function tidaklulus($body){
+          $result['success'] = false;
+
+          $this->db->where('username', $body['username']);
+          $this->db->update('t_mitra', array( 'status' => 'S3' ));
+
+          if($this->db->affected_rows() > 0){
+               $result['success'] = true;
+          }
+
+          return $result;
+     }
 }
 ?>
